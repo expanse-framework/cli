e75b7bcd521301ef0d38880ece91a01c7999ec04
Sébastien Eustace <sebastien@eustace.io>
2025-05-25 19:16:36 +0200

    Add support for specifying a Git reference in the new command

--- tests/commands/test_new.py
+++ tests/commands/test_new.py
@@ -1,14 +1,16 @@
 from pathlib import Path
 
 from cleo.testers.command_tester import CommandTester
+from pytest_mock import MockerFixture
 
 from expanse_cli.main import app
 
 
-def test_new_command(tmp_path: Path) -> None:
+def test_new_command(tmp_path: Path, mocker: MockerFixture) -> None:
     """
     The new command creates a new expanse project.
     """
+    run_copy = mocker.patch("copier.run_copy")
     command = app.find("new")
     tester = CommandTester(command)
 
@@ -29,4 +31,29 @@ def test_new_command(tmp_path: Path) -> None:
 • Creating a new Expanse project at {tmp_path}...\x1b[1G\x1b[2K\
 • Created a new Expanse project at {tmp_path}.
 """
+    )
+
+    run_copy.assert_called_once_with(
+        command.REPOSITORY_URL,
+        str(tmp_path),
+        vcs_ref="HEAD",
+        quiet=True,
+    )
+
+
+def test_new_command_with_reference(tmp_path: Path, mocker: MockerFixture) -> None:
+    """
+    The new command creates a new expanse project based on Git reference.
+    """
+    run_copy = mocker.patch("copier.run_copy")
+    command = app.find("new")
+    tester = CommandTester(command)
+
+    assert tester.execute(f"{tmp_path} -r 1.x") == 0
+
+    run_copy.assert_called_once_with(
+        command.REPOSITORY_URL,
+        str(tmp_path),
+        vcs_ref="1.x",
+        quiet=True,
     )
