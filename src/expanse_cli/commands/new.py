d3e766243a3c2c5482bea03332b0aba71b2d2bc8
Sébastien Eustace <sebastien@eustace.io>
2025-05-25 19:20:16 +0200

    Add support for specifying a Git reference in the new command (#3)

--- src/expanse_cli/commands/new.py
+++ src/expanse_cli/commands/new.py
@@ -2,7 +2,9 @@ from typing import ClassVar
 
 from cleo.commands.command import Command
 from cleo.helpers import argument
+from cleo.helpers import option
 from cleo.io.inputs.argument import Argument
+from cleo.io.inputs.option import Option
 
 
 class NewCommand(Command):
@@ -12,6 +14,17 @@ class NewCommand(Command):
 
     arguments: ClassVar[list[Argument]] = [
         argument("path", "The path where the new project will be created.")
+    ]
+
+    options: ClassVar[list[Option]] = [
+        option(
+            "reference",
+            "r",
+            "The Git reference to use for the new project. "
+            "It can be a tag, a branch or a commit hash",
+            flag=False,
+            default="HEAD",
+        ),
     ]
 
     REPOSITORY_URL = "https://github.com/expanse-framework/app.git"
@@ -33,7 +46,9 @@ class NewCommand(Command):
             f"<fg=blue>•</> Creating a new Expanse project at <comment>{path}</comment>..."
         )
 
-        run_copy(self.REPOSITORY_URL, path, vcs_ref="HEAD", quiet=True)
+        run_copy(
+            self.REPOSITORY_URL, path, vcs_ref=self.option("reference"), quiet=True
+        )
 
         self.overwrite(
             f"<fg=green>•</> Created a new Expanse project at <comment>{path}</comment>.\n"
